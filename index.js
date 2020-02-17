ca1edd0352ceac46d14d49b2f97e2a8f30f9bb61
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-17 00:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
+function moveDodgerRight(){
+  let rightStr = dodger.style.right.replace('px', '');
+  let right = parseInt(rightStr, 10);
+  if (right > 0){
+    dodger.style.right = `${right-1}px`;
+  }
+}
+
+function moveDodgerLeft(){
+  let leftStr = dodger.style.left.replace('px', '');
+  let left = parseInt(leftStr, 10);
+  if (left > 0){
+    dodger.style.left = `${left-1}px`;
+  }
+}
+
+document.addEventListener('keydown', (event)=>{
+  if (event.which === 37){
+    moveDodgerLeft()
+  }
+  else if (event.which === 39) {
+    moveDodgerRight()
+  }
+})
+
